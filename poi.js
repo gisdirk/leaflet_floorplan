36dde60be415925b9f10c729e9c9b3e7ef4c1d2a
gisdirk <72298350+gisdirk@users.noreply.github.com>
2020-11-10 15:59:46 +0700

    Create poi.js

---
+++ poi.js
@@ -0,0 +1,39 @@
+var poi =
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.61364936828613,
+          41.86761138319021
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.61673927307127,
+          41.86227407714761
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.6188850402832,
+          41.86912939783236
+        ]
+      }
+    }
+  ]
+}
